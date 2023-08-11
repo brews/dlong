e0c62dfed17a2890954e1691f5e78fb92fd63caa
Brewster Malevich <bmalevich@rhg.com>
2023-08-11 11:03:16 -0700

    Minor style cleanup

--- src/dlong/__init__.py
+++ src/dlong/__init__.py
@@ -1,5 +1,4 @@
-# noqa: F401
-from dlong.core import QuadraticDamageModel
-from dlong.core import FractionalDiscount
-from dlong.core import ExampleRecipe
-from dlong._version import __version__
+from dlong._version import __version__  # noqa: F401
+from dlong.core import QuadraticDamageModel  # noqa: F401
+from dlong.core import FractionalDiscount  # noqa: F401
+from dlong.core import ExampleRecipe  # noqa: F401
